3f28d15de6e7348b17cc634a94d1f18ee4994f65
Nguyen Son Tung <sontung2000ct@gmail.com>
2026-05-08 14:44:00 +0700

    feat: refactor hooks export structure and update ParticipantCard text color

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,23 +1,12 @@
 export { useAuth } from "./useAuth";
 export { useColorScheme } from "./useColorScheme";
 export { useCurrency } from "./useCurrency";
-export { useAddExpense, useDeleteExpense, useGetExpenses } from "./useExpense";
-export { useAcceptInvite, useCreateInvite, useInvite } from "./useInvite";
+export * from "./useExpense";
+export * from "./useInvite";
 export { useIsMobile } from "./useIsMobile";
-export {
-  useAddParticipant,
-  useCheckIsParticipant,
-  useRemoveParticipant,
-} from "./useParticipant";
+export * from "./useParticipant";
 export { useTripActions } from "./useTripActions";
-export {
-  useCreateTrip,
-  useDeleteTrip,
-  useEndTrip,
-  useTrip,
-  useTrips,
-  useUpdateTrip,
-} from "./useTrips";
+export * from "./useTrips";
 export { useTripSettlement } from "./useTripSettlement";
 export { useUserStore } from "./useUserStore";
 export { useVibrate } from "./useVibrate";
